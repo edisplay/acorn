1e768eceed60afa2230a04652d645661fc9cbb6b
cc0a <jmckeeco84@gmail.com>
2019-09-20 04:02:58 -0600

    desktop updates

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,7 +5,76 @@ export class Menu extends Component {
     render() {
         return (
             <div className="menu">
-                <h4>Menu</h4>
+                <div className="column" id="menu1">
+                    <div className="card">
+                        <div className="card" id="cardInner1">
+                        <br/>
+                        <h2 className="lunchHeading">Lunch</h2>
+                        <p className="menuItems">Candied Pecans.......................................$3.50</p>
+                        <p className="menuItems">Pork Rinds...............................................$7.50</p>
+                        <p className="menuItems">Green Olives............................................$7.50</p>
+                        <p className="menuItems">Heirloom Blue Cornbread......................$10.50</p>
+                        <p className="menuItems">Local Chicory.........................................$14.50</p>
+                        <p className="menuItems">Red Shrimp Curry..................................$18.50</p>
+                        <p className="menuItems">Oak-Roasted Monkfish..........................$17.50</p>
+                        <p className="menuItems">Moroccan Fried Chicken.......................$15.50</p>
+                        <p className="menuItems">Pork Belly Pastrami...............................$15.50</p>
+                        <p className="menuItems">Wagyu Beef...........................................$16.50</p>
+                        <p className="menuItems">Oak-Grilled Double Cheeseburger........$15.50</p>
+                        <p className="menuItems">Butter Beans..........................................$16.50</p>
+                        <p className="menuItems">Kale and Apple Salad............................$14.50</p>
+                        </div>
+                    </div>
+                    <div className="card" id="innest1">
+                        </div>
+                </div>
+                <div className="column" id="menu2">
+                <div className="card">
+                <div className="card" id="cardInner2">
+                <br/>
+                        <h2 className="lunchHeading">Lunch</h2>
+                        <p className="menuItems">Candied Pecans.......................................$3.50</p>
+                        <p className="menuItems">Pork Rinds...............................................$7.50</p>
+                        <p className="menuItems">Green Olives............................................$7.50</p>
+                        <p className="menuItems">Heirloom Blue Cornbread......................$10.50</p>
+                        <p className="menuItems">Local Chicory.........................................$14.50</p>
+                        <p className="menuItems">Red Shrimp Curry..................................$18.50</p>
+                        <p className="menuItems">Oak-Roasted Monkfish..........................$17.50</p>
+                        <p className="menuItems">Moroccan Fried Chicken.......................$15.50</p>
+                        <p className="menuItems">Pork Belly Pastrami...............................$15.50</p>
+                        <p className="menuItems">Wagyu Beef...........................................$16.50</p>
+                        <p className="menuItems">Oak-Grilled Double Cheeseburger........$15.50</p>
+                        <p className="menuItems">Butter Beans..........................................$16.50</p>
+                        <p className="menuItems">Kale and Apple Salad............................$14.50</p>
+                        </div>
+                    </div>
+                    <div className="card" id="innest2">
+                        </div>
+                </div>
+                <div className="column" id="menu3">
+                <div className="card">
+                <div className="card" id="cardInner3">
+
+                <br/>
+                        <h2 className="lunchHeading">Lunch</h2>
+                        <p className="menuItems">Candied Pecans.......................................$3.50</p>
+                        <p className="menuItems">Pork Rinds...............................................$7.50</p>
+                        <p className="menuItems">Green Olives............................................$7.50</p>
+                        <p className="menuItems">Heirloom Blue Cornbread......................$10.50</p>
+                        <p className="menuItems">Local Chicory.........................................$14.50</p>
+                        <p className="menuItems">Red Shrimp Curry..................................$18.50</p>
+                        <p className="menuItems">Oak-Roasted Monkfish..........................$17.50</p>
+                        <p className="menuItems">Moroccan Fried Chicken.......................$15.50</p>
+                        <p className="menuItems">Pork Belly Pastrami...............................$15.50</p>
+                        <p className="menuItems">Wagyu Beef...........................................$16.50</p>
+                        <p className="menuItems">Oak-Grilled Double Cheeseburger........$15.50</p>
+                        <p className="menuItems">Butter Beans..........................................$16.50</p>
+                        <p className="menuItems">Kale and Apple Salad............................$14.50</p>
+                        </div>
+                    </div>
+                    <div className="card" id="innest3">
+                        </div>
+                </div>
             </div>
         )
     }
--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import '../../sass/_reservations.scss';
 
 export class Reservations extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        const script = document.createElement("script");
+
+        script.src = "//www.opentable.com/widget/reservation/loader?rid=412810&type=standard&theme=wide&iframe=true&overlay=false&domain=com&lang=en-US";
+        script.async = true;
+
+        document.body.appendChild(script);
+    }
+
     render() {
         return (
             <div className="reservations">
-                <h4>Reservations</h4>
+                <div className="resHeading">
+                    <h2>Reservations</h2>
+                    <div className="divider"></div>
+                    <div className="openTable">
+                        <script className="openTable" type='text/javascript' src='//www.opentable.com/widget/reservation/loader?rid=412810&type=standard&theme=wide&iframe=true&overlay=false&domain=com&lang=en-US'></script>
+                    </div>
+                </div>
+                <h1 className="menuHeading">Menu</h1>
             </div>
         )
     }
