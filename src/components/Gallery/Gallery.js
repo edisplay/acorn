192c4aa25b712e53617d80bd42e3bd0e2e79dc70
cc0a <jmckeeco84@gmail.com>
2019-09-23 04:26:40 -0600

    desktop

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Image1 from './img/gal-1.jpg';
 import Image2 from './img/gal-2.png';
 import Image3 from './img/gal-3.png';
@@ -12,13 +14,13 @@ export class Gallery extends Component {
     render() {
         return (
             <div className="gallery">
-                <img src={Image1} alt="Gallery image 1" className="galleryImg" id="galleryImg1" />
-                <img src={Image2} alt="Gallery image 2" className="galleryImg" id="galleryImg2" />
-                <img src={Image3} alt="Gallery image 3" className="galleryImg" id="galleryImg3" />
-                <img src={Image4} alt="Gallery image 4" className="galleryImg" id="galleryImg4" />
-                <img src={Image5} alt="Gallery image 5" className="galleryImg" id="galleryImg5" />
-                <img src={Image6} alt="Gallery image 6" className="galleryImg" id="galleryImg6" />
-                <img src={Image7} alt="Gallery image 7" className="galleryImg" id="galleryImg7" />
+                <img src={Image1} data-aos='fade-down' alt="Gallery image 1" className="galleryImg" id="galleryImg1" />
+                <img src={Image2} data-aos='fade-up' alt="Gallery image 2" className="galleryImg" id="galleryImg2" />
+                <img src={Image3} data-aos='fade-up' alt="Gallery image 3" className="galleryImg" id="galleryImg3" />
+                <img src={Image4} data-aos='fade-right' alt="Gallery image 4" className="galleryImg" id="galleryImg4" />
+                <img src={Image5} data-aos='fade-left' alt="Gallery image 5" className="galleryImg" id="galleryImg5" />
+                <img src={Image6} data-aos='fade-up' alt="Gallery image 6" className="galleryImg" id="galleryImg6" />
+                <img src={Image7} data-aos='fade-down' alt="Gallery image 7" className="galleryImg" id="galleryImg7" />
             </div>
         )
     }
