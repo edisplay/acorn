676443a0db5c82c0c0a70770fa60d0f37fd6fa75
cc0a <jmckeeco84@gmail.com>
2019-09-23 02:30:54 -0600

    fix?

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import '../../sass/_footer.scss';
 
 export class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                <p className="footerMessage">Denver Acorn 2019 - Proudly Brought to you by <a href="http://lusciden.com">Lusciden</a></p>
+                <p className="footerMessage">Denver Acorn 2019 - Proudly Brought to you by <a href="http://lusciden.com
+                ">Lusciden</a></p>
             </div>
         )
     }
--- src/components/Map/Marker.tsx
+++ src/components/Map/Marker.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Marker.scss';
 
 const Marker = (props: any) => {
