15ad1f298292aaf32b8593147fcec63040346b35
cc0a <jmckeeco84@gmail.com>
2019-09-24 04:33:46 -0600

    mobile

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -18,15 +18,7 @@ export class Process extends Component {
     render() {
         return (
             <div className="process">
-                <div className="processDiv">
-                </div>
-                <h1 className="processHeading" data-aos='fade-down'>Our Process</h1>
-                <p className="processSummary" data-aos='fade-down'>Locally-sourced, fresh ingredients come together under the thoughtful, creative care of our chefs. Gifts of nature prepared and arranged so as to emphasize their natural flavors in a way that creates novel experiences for the palate.</p>
-                <div className="processButton" data-aos='fade-down'></div>
-                <div className="greyBox"></div>
-                <img className="dish" data-aos="fade-down" src={Dish}/>
-                <img className="chef" data-aos="fade-up" src={Chef}/>
-                <div className="button">Learn More</div>
+
             </div>
         )
     }
