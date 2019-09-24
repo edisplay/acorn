a6cbdec99770dea3635e488122e161b25e767cac
cc0a <jmckeeco84@gmail.com>
2019-09-24 16:53:03 -0600

    mobile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './sass/_base.scss';
 import Header from './components/Header/Header';
 import Process from './components/Process/Process';
 import Gsaphero from './components/Gsaphero/Gsaphero';
-// import Reservations from './components/Reservations/Reservations';
+import Reservations from './components/Reservations/Reservations';
 // import Menu from './components/Menu/Menu';
 // import Seasonal from './components/Seasonal/Seasonal'; // Test on S3 for mobile WITH Wordpress deploy!...
 // import Polygon from './components/Polygon/Polygon';
@@ -18,8 +18,8 @@ function App() {
       <Header className= "header"/>
       <Process className= "process"/>
       <Gsaphero className="gsaphero"/>
-      {/* <Reservations className="reservations"/>
-      <Menu className="menu"/>
+      <Reservations className="reservations"/>
+      {/* <Menu className="menu"/>
       <Seasonal className="seasonal"/>
       <Polygon />
       <Gallery className="gallery"/>
