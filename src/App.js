52a64a88b6f4fa7783347ac46712ac254a7fbaa3
cc0a <jmckeeco84@gmail.com>
2019-09-24 17:02:53 -0600

    mobile

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Header from './components/Header/Header';
 import Process from './components/Process/Process';
 import Gsaphero from './components/Gsaphero/Gsaphero';
 import Reservations from './components/Reservations/Reservations';
-// import Menu from './components/Menu/Menu';
-// import Seasonal from './components/Seasonal/Seasonal'; // Test on S3 for mobile WITH Wordpress deploy!...
-// import Polygon from './components/Polygon/Polygon';
-// import Gallery from './components/Gallery/Gallery';
-// import Hours from './components/Hours/Hours';
-// import Footer from './components/Footer/Footer';
+import Menu from './components/Menu/Menu';
+import Seasonal from './components/Seasonal/Seasonal'; // Test on S3 for mobile WITH Wordpress deploy!...
+import Polygon from './components/Polygon/Polygon';
+import Gallery from './components/Gallery/Gallery';
+import Hours from './components/Hours/Hours';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -19,12 +19,12 @@ function App() {
       <Process className= "process"/>
       <Gsaphero className="gsaphero"/>
       <Reservations className="reservations"/>
-      {/* <Menu className="menu"/>
+      <Menu className="menu"/>
       <Seasonal className="seasonal"/>
       <Polygon />
       <Gallery className="gallery"/>
       <Hours className="hours"/>
-      <Footer className="footer"/> */}
+      <Footer className="footer"/>
     </div>
   );
 }
