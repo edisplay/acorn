ea73d85b6ee4b3b0499b5aa7b73603995d68c6b9
cc0a <jmckeeco84@gmail.com>
2019-09-24 03:10:58 -0600

    mobile

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -24,8 +24,8 @@ export class Process extends Component {
                 <p className="processSummary" data-aos='fade-down'>Locally-sourced, fresh ingredients come together under the thoughtful, creative care of our chefs. Gifts of nature prepared and arranged so as to emphasize their natural flavors in a way that creates novel experiences for the palate.</p>
                 <div className="processButton" data-aos='fade-down'></div>
                 <div className="greyBox"></div>
-                <img className="dish" data-aos="fade-up-right" src={Dish}/>
-                <img className="chef" data-aos="fade-up-left" src={Chef}/>
+                <img className="dish" data-aos="fade-down" src={Dish}/>
+                <img className="chef" data-aos="fade-down" src={Chef}/>
                 <div className="button">Learn More</div>
             </div>
         )
