e43b8f0b01cb76dab80d1a4cb2572ea39a76eaab
cc0a <jmckeeco84@gmail.com>
2019-09-24 03:46:03 -0600

    mobile

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './App.css';
 import './sass/_base.scss';
 import Header from './components/Header/Header';
-import Process from './components/Process/Process';
-import Gsaphero from './components/Gsaphero/Gsaphero';
-import Reservations from './components/Reservations/Reservations';
-import Menu from './components/Menu/Menu';
-import Seasonal from './components/Seasonal/Seasonal';
-import Polygon from './components/Polygon/Polygon';
-import Gallery from './components/Gallery/Gallery';
-import Hours from './components/Hours/Hours';
-import Footer from './components/Footer/Footer';
+// import Process from './components/Process/Process';
+// import Gsaphero from './components/Gsaphero/Gsaphero';
+// import Reservations from './components/Reservations/Reservations';
+// import Menu from './components/Menu/Menu';
+// import Seasonal from './components/Seasonal/Seasonal';
+// import Polygon from './components/Polygon/Polygon';
+// import Gallery from './components/Gallery/Gallery';
+// import Hours from './components/Hours/Hours';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="container">
       <Header className= "header"/>
-      <Process className= "process"/>
+      {/* <Process className= "process"/>
       <Gsaphero className="gsaphero"/>
       <Reservations className="reservations"/>
       <Menu className="menu"/>
@@ -24,7 +24,7 @@ function App() {
       <Polygon />
       <Gallery className="gallery"/>
       <Hours className="hours"/>
-      <Footer className="footer"/>
+      <Footer className="footer"/> */}
     </div>
   );
 }
