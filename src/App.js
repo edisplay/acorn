d49c5f4ca0a2d002135fa57c4cfdfec983c2d895
cc0a <jmckeeco84@gmail.com>
2019-09-25 14:29:42 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 import './sass/_base.scss';
 import Header from './components/Header/Header';
 import Process from './components/Process/Process';
-// import Gsaphero from './components/Gsaphero/Gsaphero';
-// import Reservations from './components/Reservations/Reservations';
-// import Menu from './components/Menu/Menu';
-// import Seasonal from './components/Seasonal/Seasonal'; // Test on S3 for mobile WITH Wordpress deploy!...
-// import Gallery from './components/Gallery/Gallery';
-// import Hours from './components/Hours/Hours';
-// import Footer from './components/Footer/Footer';
+import Gsaphero from './components/Gsaphero/Gsaphero';
+import Reservations from './components/Reservations/Reservations';
+import Menu from './components/Menu/Menu';
+import Seasonal from './components/Seasonal/Seasonal'; // Test on S3 for mobile WITH Wordpress deploy!...
+import Gallery from './components/Gallery/Gallery';
+import Hours from './components/Hours/Hours';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="container">
       <Header className= "header"/>
       <Process className= "process"/>
-      {/* <Gsaphero className="gsaphero"/> */}
-      {/* <Reservations className="reservations"/>
+      <Gsaphero className="gsaphero"/>
+      <Reservations className="reservations"/>
       <Menu className="menu"/>
       <Seasonal className="seasonal"/>
       <Gallery className="gallery"/>
       <Hours className="hours"/>
-      <Footer className="footer"/> */}
+      <Footer className="footer"/>
     </div>
   );
 }
