1ded6ca7ffebf289373ffbc6164d67bdc0f63b5e
cc0a <jmckeeco84@gmail.com>
2019-09-24 19:12:51 -0600

    mobile

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -25,9 +25,11 @@ export class Process extends Component {
                     <p className="summary">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu</p>
                     <div className="button">Learn More</div>
                 </div>
-                <div className="greyBox"></div>
+                {/* <div className="greyBox">
                     <img className="dish" data-aos="fade-up" src={Bowl}/>
                     <img className="chef" data-aos="fade-down" src={Chef2}/>
+                </div> */}
+                    
             </div>
         )
     }
