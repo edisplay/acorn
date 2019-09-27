9b843e04279e13ac0208cd920a868c69bd2be07d
cc0a <jmckeeco84@gmail.com>
2019-09-26 20:18:43 -0600

    updates

--- src/components/Employment/Employment.js
+++ src/components/Employment/Employment.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import './employment.scss';
+// import './employment.scss';
 
 export class Employment extends Component {
     render() {
@@ -8,13 +8,13 @@ export class Employment extends Component {
                 			<form action="POST" data-netlify="true" className="contactForm">
 								<div class="fields">
 									<div class="field half">
-										<input type="text" name="name" id="name" placeholder="Name" />
+										<input type="text" name="name" id="name" placeholder="Name" className="fieldCon"/>
 									</div>
 									<div class="field half">
-										<input type="email" name="email" id="email" placeholder="Email" />
+										<input type="email" name="email" id="email" placeholder="Email" className="fieldCon"/>
 									</div>
 									<div class="field">
-										<textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+										<textarea name="message" id="message" placeholder="Message" rows="7" className="fieldCon"></textarea>
 									</div>
                                     <div class="field">
 										<input type="file" name="myfile" id="myfile" placeholder="Upload File" rows="7"></input>
